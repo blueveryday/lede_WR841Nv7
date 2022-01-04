996219f13df272ebbdef92122517795aae22da6c
blueveryday <blueveryday@163.com>
2022-01-04 19:15:29 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -4,6 +4,7 @@ git clone "https://github.com/coolsnowwolf/lede.git"
 
 pushd "lede"; 
 
+git reset --hard c50e1753836fd8a85a924c0daa61193bfc69d996
 sed -i -e 's/192.168.1.1/192.168.41.1/g' -e 's/OpenWrt/TPLink_WR841Nv7/g' package/base-files/files/bin/config_generate  #修改路由器管理IP地址和主机名
 sed -i '545s/4/16/g' target/linux/ar71xx/image/tiny-tp-link.mk  #修改wr841nv7的编译固件大小
 sed -i -e 's/OpenWrt/TPLink_WR841Nv7/g' -e 's/encryption=none/encryption=psk2/g' -e '/psk2/a\			set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
