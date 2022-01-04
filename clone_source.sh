c23721d8a8dc63fbd821406a706e9eab853c7ce3
blueveryday <blueveryday@163.com>
2022-01-04 18:45:21 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -8,7 +8,11 @@ sed -i -e 's/192.168.1.1/192.168.41.1/g' -e 's/OpenWrt/TPLink_WR841Nv7/g' packag
 sed -i '545s/4/16/g' target/linux/ar71xx/image/tiny-tp-link.mk  #修改wr841nv7的编译固件大小
 sed -i -e 's/OpenWrt/TPLink_WR841Nv7/g' -e 's/encryption=none/encryption=psk2/g' -e '/psk2/a\			set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # 修改WiFi名称,添加加密方式和密码
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default                                      #使用源码自带ShadowSocksR Plus软件
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default                                      #使用源码自带ShadowSocksR Plus软件
+
+cd package
+git clone https://github.com/fw876/helloworld.git
+cd ..
 
 sed -i -e '1s/$/^e82d730/g' feeds.conf.default
 sed -i -e '2s/$/^974fb04/g' feeds.conf.default
